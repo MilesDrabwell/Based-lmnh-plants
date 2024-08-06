fa18953e7943d36be56ff12b910db17453ffde47
HighestAuto <Jonathan.at.spence@hotmail.com>
2024-08-06 12:34:23 +0100

    merge error fixes

--- fetch_data.py
+++ fetch_data.py
@@ -11,9 +11,12 @@ PLANT_IDS = list(range(51))
 
 async def get_plant_data(plant_ids: list) -> list[dict]:
     """Asynchronous function to retrieve the plant data for multiple plants"""
+    if not isinstance(plant_ids, list):
+        raise TypeError
     plants_data = []
     async with aiohttp.ClientSession() as session:
-        tasks = [session.get(URL.format(plant_id), ssl=False) for plant_id in plant_ids]
+        tasks = [session.get(URL.format(plant_id), ssl=False)
+                 for plant_id in plant_ids]
         responses = await asyncio.gather(*tasks)
 
         for response in responses:
@@ -23,7 +26,7 @@ async def get_plant_data(plant_ids: list) -> list[dict]:
     return plants_data
 
 
-def new_plant_ids(plants_data: list[dict]):
+def new_plant_ids(plants_data: list[dict]) -> list[int]:
     """Function to generate plant IDs for the next iteration"""
     plant_ids = []
     for plant_data in plants_data:
