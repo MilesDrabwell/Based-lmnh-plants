140cdf8a327bc5a9b9ad8cfd9f9bbc45160022cb
MilesDrabwell <163558169+MilesDrabwell@users.noreply.github.com>
2024-08-08 11:35:20 +0100

    deleted some nulls

--- long_term_transfer/main.tf
+++ long_term_transfer/main.tf
@@ -6,27 +6,14 @@ provider "aws" {
 
 resource "aws_lambda_function" "historic_lambda" {
   architectures                      = ["x86_64"]
-  code_signing_config_arn            = null
-  description                        = null
-  filename                           = null
   function_name                      = "c12-based-update-historic-data"
-  handler                            = null
   image_uri                          = "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c12-based-lambda:latest"
-  kms_key_arn                        = null
   layers                             = []
   memory_size                        = 128
   package_type                       = "Image"
-  publish                            = null
-  replace_security_groups_on_destroy = null
-  replacement_security_group_ids     = null
   reserved_concurrent_executions     = -1
   role                               = "arn:aws:iam::129033205317:role/service-role/based-update-historic-data-role-nh0kc4vk"
-  runtime                            = null
-  s3_bucket                          = null
-  s3_key                             = null
-  s3_object_version                  = null
   skip_destroy                       = false
-  source_code_hash                   = null
   tags                               = {}
   tags_all                           = {}
   timeout                            = 3
@@ -43,10 +30,8 @@ resource "aws_lambda_function" "historic_lambda" {
     size = 512
   }
   logging_config {
-    application_log_level = null
     log_format            = "Text"
     log_group             = "/aws/lambda/based-update-historic-data"
-    system_log_level      = null
   }
   tracing_config {
     mode = "PassThrough"
@@ -54,15 +39,10 @@ resource "aws_lambda_function" "historic_lambda" {
 }
 
 resource "aws_scheduler_schedule" "lambda_schedule" {
-  description                  = null
-  end_date                     = null
   group_name                   = "default"
-  kms_key_arn                  = null
   name                         = "c12-based-historic-data-upload"
-  name_prefix                  = null
   schedule_expression          = "cron(0 0 * * ? *)"
   schedule_expression_timezone = "UTC"
-  start_date                   = null
   state                        = "ENABLED"
   flexible_time_window {
     maximum_window_in_minutes = 15
@@ -70,7 +50,6 @@ resource "aws_scheduler_schedule" "lambda_schedule" {
   }
   target {
     arn      = "arn:aws:lambda:eu-west-2:129033205317:function:c12-based-update-historic-data"
-    input    = null
     role_arn = "arn:aws:iam::129033205317:role/service-role/Amazon_EventBridge_Scheduler_LAMBDA_ea33825b28"
     retry_policy {
       maximum_event_age_in_seconds = 86400
