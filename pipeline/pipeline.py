1460e9c0a99985f04871978db0b858130d6f98bb
LinfanS <trainee.linfan.siddiqi@sigmalabs.co.uk>
2024-08-08 12:23:07 +0100

    switched pipeline for AWS Lambda instead

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -14,43 +14,33 @@ logger = logging.getLogger()
 URL = "https://data-eng-plants-api.herokuapp.com/plants/{}"
 PLANT_IDS = list(range(1, 51))
 
-url = "https://data-eng-plants-api.herokuapp.com/plants/{}"
 
-
-def get_plant_data_slow() -> list[dict]:
-
-    plant_ids = range(1)
-    results = []
-    for plant_id in plant_ids:
-        response = requests.get(url.format(plant_id))
-        results.append(response.json())
-    return results
-
-
-async def main() -> None:
+async def main() -> dict:
     """Main function to run the script in a while loop"""
     plant_ids = PLANT_IDS
     conn = get_connection()
-    while True:
-        start_time = time.time()
-        logger.info("fetching plant data from api")
-        plants_data = await get_api_plant_data(plant_ids)
-        plant_ids = new_plant_ids(plants_data)
-        logger.info("plant data acquired from api")
-        logger.info("transforming plant data ready for upload")
-        transformed = get_table_data(plants_data, conn)
-        logger.info("transformed plant data uploading")
-        load(conn, transformed)
-        logger.info("all plant data uploaded")
-        end_time = time.time()
-        duration = end_time - start_time
-        logger.info(f"API calls took {duration} seconds")
-        if duration < 60:
-            time.sleep(60 - duration)
+
+    start_time = time.time()
+    logger.info("fetching plant data from api")
+    plants_data = await get_api_plant_data(plant_ids)
+    plant_ids = new_plant_ids(plants_data)
+    logger.info("plant data acquired from api")
+    logger.info("transforming plant data ready for upload")
+    transformed = get_table_data(plants_data, conn)
+    logger.info("transformed plant data uploading")
+    load(conn, transformed)
+    logger.info("all plant data uploaded")
+    end_time = time.time()
+    duration = end_time - start_time
+    # logger.info(f"API call took {duration} seconds")
+    return duration
+
+
+def handler(event: dict, context) -> dict:
+    duration = asyncio.run(main())
+    return {"message": f"API call took {duration} seconds"}
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    print("hellllllloooooo wooooorllllllddddddd")
-    print(get_plant_data_slow())
-    asyncio.run(main())
+    print(handler({}, None))
