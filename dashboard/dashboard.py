70ce3e3716594f362d5e666b2f839286025d177c
annac02 <anna.camplani@gmial.com>
2024-08-08 11:17:35 +0100

    WORKS with botanist filter

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -20,11 +20,9 @@ def get_connection():
     )
 
 #get all the variables required for the filters
-def get_botanists():
+def get_botanists(conn):
     """Obtains data about botanists from the RDS database"""
-    load_dotenv()
-    conn = get_connection()
-    query = """SELECT * FROM alpha.botanist;"""
+    query = """SELECT * FROM alpha.botanist"""
     with conn.cursor() as cur:
         cur.execute(query)
         all_data = cur.fetchall()
@@ -37,27 +35,7 @@ def get_time_range():
 def get_origin_countries():
     pass
 
-
-#TODO - change based on filtered data instead
-def get_temp_vs_moist(conn):
-    now = datetime.now()
-    a_minute_ago = (now - timedelta(minutes=700)).strftime("%Y-%m-%d %H:%M")
-    print('minute', a_minute_ago)
-    now = (datetime.strptime(a_minute_ago,"%Y-%m-%d %H:%M") + timedelta(minutes=1)).strftime("%Y-%m-%d %H:%M")
-    print('now', now)
-    query = """
-    SELECT plant_id, soil_moisture, temperature 
-    FROM alpha.plant_health
-    WHERE recording_time > %s AND recording_time < %s;
-    """
-    with conn.cursor() as cur:
-        cur.execute(query,(a_minute_ago, now)) #cur.execute(query,a_minute_ago, now)
-        all_data = cur.fetchall()
-    conn.commit()
-    data_df = pd.DataFrame(all_data, columns =['plant_id', 'soil_moisture', 'temperature'])
-    return data_df
-
-def temp_vs_moist(data):
+def temp_vs_moist(data:pd.DataFrame):# -> alt.Chart? type says <class 'altair.vegalite.v5.api.Chart'>
     """
     Scatter graph showing the relationship between temperature and soil moisture
     """
@@ -68,60 +46,87 @@ def temp_vs_moist(data):
 
     return moist_temp
 
-def outliers(data):
+def outliers(data:pd.DataFrame)->tuple[pd.DataFrame]:
     #decided based on average values
     soil_outliers = (data['soil_moisture'] < 10) | ((data['soil_moisture'] > 30) & (data['soil_moisture'] < 95))
     temp_outliers = (data['temperature'] < 8) | (data['temperature'] > 31)
     return (data.loc[soil_outliers], data.loc[temp_outliers])
 
-def warnings(data):
+def warnings(data:tuple[pd.DataFrame]):
+    """
+    Displays warnings for any plants that need attention
+    Has these as a list with checkboxes so that you can keep track of which plants you have checked
+    (the input 'data' is a tuple, data[0] is the outliers based on the moisture, data[1] is based ont he temperature)
+    """
     st.subheader(':rotating_light: **:red[WARNINGS:]**', help='Plants that need checking')
     for idx, i in data[0].iterrows():
-        st.checkbox(f"""**Plant with ID {i['plant_id'].item()} has soil moisture: {round(i['soil_moisture'].item(),2)}**""")
+        st.checkbox(f"""**Plant with ID {i['plant_id']} has soil moisture: {round(i['soil_moisture'],2)}**""")
     for idx, i in data[1].iterrows():
-        st.checkbox(f"""**Plant with ID {i['plant_id'].item()} has temperature: {i['temperature'].item()}**""")
+        st.checkbox(f"""**Plant with ID {i['plant_id']} has temperature: {i['temperature']}**""")
 
-def filter_data(conn):
-    all_data = """
-    SELECT b.name, o.continent_name, p.plant_name, ph.recording_time, ph.soil_moisture, ph.temperature, ph.last_watered, ph.plant_id
-    FROM alpha.botanist b, alpha.origin_location o, alpha.plant p, alpha.plant_health ph
-    WHERE b.botanist_id = p.botanist_id AND o.origin_location_id=p.origin_location_id AND ph.plant_id=p.plant_id
+def filter_data(conn, live=True):
     """
-    original_data = all_data
-    botanist_choices = [i[1] for i in get_botanists()]
+    Returns only the data that has been filtered based on the options chosen through the sidebar
+    """
+    all_data = """
+    WITH all_data AS(
+        SELECT b.name, o.continent_name, p.plant_name, ph.recording_time, ph.soil_moisture, ph.temperature, ph.last_watered, ph.plant_id
+        FROM alpha.botanist b, alpha.origin_location o, alpha.plant p, alpha.plant_health ph
+        WHERE b.botanist_id = p.botanist_id AND o.origin_location_id=p.origin_location_id AND ph.plant_id=p.plant_id
+        )
+    """
+    data = 'all_data'
+
+    botanist_choices = [i[1] for i in get_botanists(conn)]
     botanist_selected = st.sidebar.multiselect(
         "Filter by Botanist", botanist_choices)
     if botanist_selected:
-        all_data = """
-        WITH botanist_filter AS(%s)
-        SELECT *
-        FROM botanist_filter
-        WHERE name in %s;
+        if len(botanist_selected) == 1:
+            botanist_selected = f"('{botanist_selected[0]}')"
+        else:
+            botanist_selected = tuple(botanist_selected)
+        all_data = f"""{all_data},
+        botanist_data AS(
+            SELECT *
+            FROM {data}
+            WHERE name in {botanist_selected})
         """
+        data = 'botanist_data'
 
+    if live:
+        now = datetime.now()
+        a_minute_ago = (now - timedelta(minutes=1240)).strftime("%Y-%m-%d %H:%M")
+        print('minute', a_minute_ago)
+        now = (datetime.strptime(a_minute_ago,"%Y-%m-%d %H:%M") + timedelta(minutes=1)).strftime("%Y-%m-%d %H:%M")
+        print('now', now)
+        print('data from',data)
+        all_data = f"""{all_data},
+        live_data AS(
+            SELECT *
+            FROM {data}
+            WHERE recording_time > '2024-08-07 13:48' AND recording_time < '2024-08-07 13:49')
+        """
+        data = 'live_data'
 
-    
-        with conn.cursor() as cur:
-            cur.execute(all_data,(original_data,*botanist_selected))
-            data_points = cur.fetchall()
-        conn.commit()
-        data_df = pd.DataFrame(data_points, columns =['name', 'continent_name', 'plant_name', 'recording_time',
-                                                    'soil_moisture', 'temperature', 'last_watered', 'plant_id'])
-    
-
-        return data_df
+    query = f"""{all_data} SELECT * FROM {data};"""
+    with conn.cursor() as cur:
+        cur.execute(query)
+        data_points = cur.fetchall()
+    conn.commit()
+    data_df = pd.DataFrame(data_points, columns =['name', 'continent_name', 'plant_name', 'recording_time',
+                                                        'soil_moisture', 'temperature', 'last_watered', 'plant_id'])
+    return data_df
 
 if __name__ == "__main__":
     load_dotenv()
     connection = get_connection()
-    #print(filter_data(connection))
     tab1, tab2 = st.tabs(["Live", "Historical"])
 
     with tab1:
-        st.sidebar.multiselect('Choose a botanist',['a','b','c'])
-        errors = outliers(get_temp_vs_moist(connection))
+        live_data = filter_data(connection)
+        errors = outliers(live_data)
         if not (errors[0].empty and errors[1].empty):
             warnings(errors)
         st.header("Temperature and Moisture of the last minute")
-        graph = temp_vs_moist(get_temp_vs_moist(connection))
+        graph = temp_vs_moist(live_data)
         st.altair_chart(graph, use_container_width=True)
