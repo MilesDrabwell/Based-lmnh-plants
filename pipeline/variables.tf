7fd459fff215cd4030046f22e34a88e3759c37b5
MilesDrabwell <163558169+MilesDrabwell@users.noreply.github.com>
2024-08-08 12:47:36 +0100

    added pipeline terraform

---
+++ pipeline/variables.tf
@@ -0,0 +1,31 @@
+variable "AWS_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_SECRET_KEY" {
+  type = string
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
+variable "DB_HOST" {
+  type = string
+}
+
+variable "DB_NAME" {
+  type = string
+}
+
+variable "DB_PASSWORD" {
+  type = string
+}
+
+variable "DB_PORT" {
+  type = string
+}
+
+variable "DB_USER" {
+  type = string
+}
