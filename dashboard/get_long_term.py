3987d71b630bc74b49a9df17966ca0c9391f27c5
HighestAuto <Jonathan.at.spence@hotmail.com>
2024-08-09 15:29:16 +0100

    typehint and docstring

--- dashboard/get_long_term.py
+++ dashboard/get_long_term.py
@@ -1,13 +1,15 @@
 from os import getenv
 from os.path import isfile
-import boto3
+from boto3 import client
 import pandas as pd
 
 BUCKET_NAME = "c12-lmnh-based-storage"
 BUCKET_KEY_NAME = 'Contents'
+
+
 def get_client():
     """Initiates a connection to the S3 AWS Cloud using required credentials."""
-    aws_client = boto3.client(
+    aws_client = client(
         "s3",
         aws_access_key_id=getenv("ACCESS_KEY_ID"),
         aws_secret_access_key=getenv("SECRET_ACCESS_KEY"),
@@ -15,7 +17,7 @@ def get_client():
     return aws_client
 
 
-def get_file_names(client, bucket_name: str) -> list[str]:
+def get_file_names(client: client, bucket_name: str) -> list[str]:
     """Gets the list of valid parquet filenames and downloads files not yet downloaded."""
     filenames = []
     objects = client.list_objects_v2(Bucket=bucket_name)
@@ -29,6 +31,7 @@ def get_file_names(client, bucket_name: str) -> list[str]:
 
     return filenames
 
+
 def combine_transaction_data_files(filenames: list[str]) -> pd.DataFrame:
     """Converts parquet files to a single pandas dataframe."""
     df = pd.DataFrame()
@@ -37,10 +40,15 @@ def combine_transaction_data_files(filenames: list[str]) -> pd.DataFrame:
         df = pd.concat([df, parquet], ignore_index=True)
     return df
 
+
 def get_long_term_data() -> pd.DataFrame:
+    """
+    Gets files from our based s3 bucket
+    """
     client = get_client()
     filenames = get_file_names(client, BUCKET_NAME)
     return combine_transaction_data_files(filenames)
+
 
 if __name__ == "__main__":
     get_long_term_data()
